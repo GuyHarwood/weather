61288bcc96ceb5e286a049aea7397955250aca98
Guy Harwood <gpharwood@gmail.com>
2014-05-24 21:29:39 +0100

    ngResource included

--- app/scripts/services/weatherService.js
+++ app/scripts/services/weatherService.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('weatherApp')
-  .factory('weatherService', function () {
+  .factory('weatherService', function ($resource) {
 
     return {
       getCurrent : function(){
+
+          $resource('http://api.openweathermap.org/data/2.5/weather?q=London,uk', {}, {
+            query: {
+              method:'GET'
+            }
+          });
+
+          console.log($resource);
 
           return {
             icon : '01d',
